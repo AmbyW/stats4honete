8ce2198bb3af563889491d7452539b177b44c16e
Reynaldo Cuenca Campos <reynaldo86.onwork@gmail.com>
2023-09-04 03:55:27 +0700

    added template context processor to add all the time preferences information in the templates

--- statshon/settings.py
+++ statshon/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'honauth.context_processors.hon_app_settings',
             ],
         },
     },

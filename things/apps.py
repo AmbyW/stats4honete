7125a85df375fbdac77eccb1582ddd8bfabdeb9d
Reynaldo Cuenca Campos <reynaldo86.onwork@gmail.com>
2023-09-04 20:53:10 +0700

    make some changes to makes run the app on heroku

--- things/apps.py
+++ things/apps.py
@@ -1,12 +1,43 @@
 from django.apps import AppConfig
+from django.conf import settings
+from django.contrib.auth import get_user_model
 
 
 class ThingsConfig(AppConfig):
     name = 'things'
 
     def ready(self):
+        check_and_create_initial_models()
+        check_and_create_default_superuser()
+
+
+def check_and_create_initial_models():
+    try:
+        from .models import TypeAttack, TypeHero, TypeTeam
+        TypeAttacks = [('Melee', '1'), ('Rango', '2'), ]
+        TypeHeroes = [('Agilidad', '1'), ('Fuerza', '2'), ('Inteligencia', '3'), ]
+        TypeTeams = [('HellBourne', '1'), ('Legion', '2'), ]
+        check_and_create_models(TypeTeam, TypeTeams)
+        check_and_create_models(TypeHero, TypeHeroes)
+        check_and_create_models(TypeAttack, TypeAttacks)
+
+    except ImportError as _:
         pass
-        # verify
-        # TypeAttack.objects.bulk_create([TypeAttack(name='Melee', code='1'), TypeAttack(name='Rango', code='2')])
-        # TypeHero.objects.bulk_create([TypeHero(name='Agilidad', code='1'), TypeHero(name='Fuerza', code='2'), TypeHero(name='Inteligencia', code='3')])
-        # TypeTeam.objects.bulk_create([TypeTeam(name='HellBourne', code='1'), TypeTeam(name='Legion', code='2')])
+
+
+def check_and_create_models(model_class, values):
+    [model_class.objects.create(name=value[0], code=value[1]) for value in values
+     if not model_class.objects.filter(name=value[0], code=value[1]).exists()]
+
+
+def check_and_create_default_superuser():
+
+    if hasattr(settings, 'DEFAULT_SUPERUSER_NAME') and settings.DEFAULT_SUPERUSER_NAME \
+            and hasattr(settings, 'DEFAULT_SUPERUSER_PASS') and settings.DEFAULT_SUPERUSER_PASS:
+        username = settings.DEFAULT_SUPERUSER_NAME
+        password = settings.DEFAULT_SUPERUSER_PASS
+        UserModel = get_user_model()
+        if not UserModel.objects.filter(username=username).exists():
+            superuser = UserModel(username=username)
+            superuser.set_password(password=password)
+            superuser.save()

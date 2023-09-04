d12fa8d0284d5934980820416a8f150c2a86d1f9
Reynaldo Cuenca Campos <reynaldo86.onwork@gmail.com>
2023-09-05 02:15:09 +0700

    added more settings

--- statshon/settings.py
+++ statshon/settings.py
@@ -99,6 +99,8 @@ DATABASES = {
     }
 }
 
+CSRF_TRUSTED_ORIGINS = env.list('DJANGO_TRUSTED_ORIGINS', default=['*'])
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
